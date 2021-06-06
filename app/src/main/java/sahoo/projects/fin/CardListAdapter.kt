9ac03e1e23a755f527dcf8b5660b20315170ac7b
Pradeep Kumar Sahoo <sahoo.pradeep.nits@gmail.com>
2021-06-06 21:00:34 +0530

    [not working] added onClickListener in card list item

--- app/src/main/java/sahoo/projects/fin/CardListAdapter.kt
+++ app/src/main/java/sahoo/projects/fin/CardListAdapter.kt
@@ -1,9 +1,10 @@
 package sahoo.projects.fin
 
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_card_list_item.view.*
 import sahoo.projects.fin.model.CardDetail
@@ -12,7 +13,18 @@ class CardListAdapter(
     private var cardDetails: List<CardDetail>,
 ) : RecyclerView.Adapter<CardListAdapter.CardDetailViewHolder>() {
 
-    inner class CardDetailViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    inner class CardDetailViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        init {
+            itemView.setOnClickListener { view ->
+                Log.d("PRADEEP", "Hello")
+                Intent(view.context, CardDetailActivity::class.java).also {
+                    it.putExtra("EXTRA_CARD_DETAIL", cardDetails[absoluteAdapterPosition])
+                    it.putExtra("EXTRA_IS_NEW_CARD", false)
+                    view.context.startActivity(it)
+                }
+            }
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardDetailViewHolder {
         val view = LayoutInflater.from(parent.context)
